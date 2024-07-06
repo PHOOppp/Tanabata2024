21d58671fe2cae70e4269d2f386a4b86aed4e45f
PHOOppp <phoodit.p@kvis.ac.th>
2024-07-06 22:52:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,33 +3,43 @@ let ovl = document.getElementById("overlay");
 let tzk = document.getElementById("card");
 let color_algo = document.getElementById("cl");
 
-            
-function openPopup(){                
-    popup.classList.add("open-popup");
-    optionTazaku.classList.add("open-option");
-    ovl.classList.add("ovl");
+var BOD=  document.querySelector('body');
+var windowWidth = window.outerWidth;
+BOD.style.height = windowWidth * 2.41 + 'px';
+
+window.addEventListener('resize', function () {
+  var backgroundImage = document.querySelector('body');
+  var windowWidth = window.outerWidth;
+  backgroundImage.style.height = windowWidth * 2.41 + 'px';
+});
+
+
+function openPopup() {
+  popup.classList.add("open-popup");
+  optionTazaku.classList.add("open-option");
+  ovl.classList.add("ovl");
 }
 
-function closePopup(){
-    ovl.classList.remove("ovl");
-    popup.classList.remove("open-popup");
-    optionTazaku.classList.remove("open-option");
+function closePopup() {
+  ovl.classList.remove("ovl");
+  popup.classList.remove("open-popup");
+  optionTazaku.classList.remove("open-option");
 }
 
- let cardColor = [
-       "black","#fdb7c2","#a1e9ff","#3f3152","#fbe1a4","#acf4b4","#d1585f","#f5f4f0"
+let cardColor = [
+  "black", "#fdb7c2", "#a1e9ff", "#3f3152", "#fbe1a4", "#acf4b4", "#d1585f", "#f5f4f0"
 ]
 
- let fontColor = [
-        "white", "#e4478c", "#005665", "#ebdbf8", "#6c2803","#007179","white","black"
+let fontColor = [
+  "white", "#e4478c", "#005665", "#ebdbf8", "#6c2803", "#007179", "white", "black"
 ]
 
-function color(z){
-    var a = getComputedStyle(z);
-    var b = a.backgroundColor;
-    var c = cardColor.indexOf(b);
-    var d = fontColor[c];
-    document.getElementById("popup").style.backgroundColor = b;
+function color(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  var c = cardColor.indexOf(b);
+  var d = fontColor[c];
+  document.getElementById("popup").style.backgroundColor = b;
 }
 
 let slideIndex = 1;
@@ -61,7 +71,7 @@ function showSlides(n) {
 
 function openPopup_story() {
   popupstory.classList.add("open-popup_story");
-    ovl.classList.add("ovl");
+  ovl.classList.add("ovl");
 }
 
 function closePopup_story() {
